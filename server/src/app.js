b20873bb423f4d2b0e562bee76517270bfeac925
Moacir <moacir.rogerio@camara.leg.br>
2018-02-15 17:52:37 -0200

    Configuração do servidor

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('No ar!')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Tá no ar!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
